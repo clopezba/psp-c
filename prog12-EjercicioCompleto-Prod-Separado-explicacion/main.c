fa7033821a6b3839899204ec06ade818738f7e58
Cristina <200101@iessierradeguara.com>
2021-11-11 17:32:07 +0100

    Casi

---
+++ prog12-EjercicioCompleto-Prod-Separado-explicacion/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#include "./funciones.h"
+
+
+bool final = false;
+
+void gestor_senyal(int senyal)
+{
+    if (senyal == SIGUSR2)
+    { //La señal 2 la usaremos para finalizar.
+        final = true;
+    }
+}
+
+int main()
+{
+    pid_t consumidor;
+
+    signal(SIGUSR1, gestor_senyal); //registramos el tratamiento de SIGUSR1
+    signal(SIGUSR2, gestor_senyal); //registramos el tratamiento de SIGUSR2
+
+    int fd;
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
+    consumidor = fork();
+    if (consumidor == -1)
+    {
+        perror("Error creando al consumidor.");
+        exit(2);
+    }
+    if (consumidor == 0)
+    { //Proceso consumidor. Sólo leerá del pipe así que...
+        unsigned int monedas_necesarias = int_aleatorio(5, 15);
+        printf("[Consumidor]: Se precisan %u monedas.\n", monedas_necesarias);
+        unsigned int monedas_disponibles = 0, monedas_recibidas;
+        
+        while (monedas_disponibles < monedas_necesarias)
+        {
+            printf("[Consumidor]: No tengo monedas suficientes. Notificando la situación al padre.\n");
+            sleep(1);
+            kill(getppid(), SIGUSR1); //Notificamos al padre que necesito monedas.
+            printf("[Consumidor]: Esperando una señal antes de leer.\n");
+            pause();
+            fd = open(myfifo, O_RDONLY | O_NONBLOCK);
+            //leo las monedas y las añado a las monedas disponibles
+            read(fd, &monedas_recibidas, sizeof(unsigned int));
+            printf("Monedas recibidas:%d\n", monedas_recibidas);
+            monedas_disponibles += monedas_recibidas; //añado las recibidas a las que ya tenía.
+            printf("[Consumidor]: Recibidas %u monedas del productor, ya tengo %u en total.\n", monedas_recibidas, monedas_disponibles);
+        }
+        //ya no voy a leer más.
+        close(fd);
+        printf("[Consumidor]: Ya puedo construir, la construcción tardará 5 segundos.\n");
+        sleep(5);
+        printf("[Consumidor]: Construcción finalizada, notificando al padre.\n");
+        kill(getppid(), SIGUSR2); //desbloqueo al padre
+        //final del código del consumidor.
+    }
+    else
+    { // Código del padre, me quedo esperando la señal inicial:
+        char buffer[6];
+        printf("[Padre]: Mi pid es %d, el pid del consumidor es %d.\n", getpid(), consumidor);
+        sprintf(buffer, "%d", consumidor);
+        pid_t productor = fork();
+        if (productor == 0)
+        {
+            if (execlp("./productor.exe", "./productor.exe", buffer, NULL) == -1)
+            {
+                perror("Error al lanzar programa");
+            }
+        }
+        /*
+        FILE *fichero = NULL;
+        char aux[6];
+
+        fichero = popen("/bin/pgrep productor", "r");
+        if (fichero == NULL)
+        {
+            perror("No se puede abrir fichero");
+            exit(-1);
+        }
+        char *pidProd = fgets(aux, 6, fichero);
+        pid_t productor = atoi(pidProd);
+        
+        pclose(fichero);
+        */
+        else
+        {
+            pause();
+            while (!final)
+            {
+                //Si he recibido una señal y no se ha acabado la construcción, es que tengo que notificar al prodcutor
+                kill(productor, SIGUSR1);
+                printf("[Padre]: Esperando una señal...\n");
+                pause(); //me quedo esperando la siguiente señal.
+            }
+            //el consumidor ha terminado, lo espero.
+            wait(NULL);
+            //enviando la señal de finali al productor para que se desbloquee actualizando su valor de final.
+            kill(productor, SIGUSR2);
+            //esperando al productor
+            wait(NULL);
+        }
+    }
+}
+
+//preguntas: En este caso. ¿Importa el orden de creación de los hijos?¿Por qué sí o por qué no?
---
+++ prog12-EjercicioCompleto-Prod-Separado-explicacion/productor.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#include "./funciones.h"
+
+#define MIN 1
+#define MAX 5
+
+bool final = false;
+
+void gestor_senyal(int senyal)
+{
+    if (senyal == SIGUSR2)
+    {
+        final = true;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    //atoi traduce una cadena a un entero
+    pid_t consumidor = atoi(argv[1]);
+    signal(SIGUSR1, gestor_senyal); //registramos el tratamiento de SIGUSR1
+    signal(SIGUSR2, gestor_senyal); //registramos el tratamiento de SIGUSR2
+    printf("[Productor]: Mi pid: %d, el pid del consumidor es %d:\n",getpid(), consumidor);
+    int fd;
+    char *myfifo = "/tmp/myfifo";
+    mkfifo(myfifo, 0666);
+
+    
+    
+    unsigned int monedas_producidas = 0;
+    printf("[Productor]: Iniciando producción bajo demanda... Esperando señal de producción\n");
+    pause();
+    
+    while (!final)
+    {
+        printf("[Productor]: Produciendo monedas, tardaré 3 segundos en producir entre %d y %d monedas.\n", MIN, MAX);
+        //genero monedas
+        sleep(3);
+        monedas_producidas = int_aleatorio(MIN, MAX);
+        printf("[Productor]: Producidas %u monedas.\n", monedas_producidas);
+        //escribo las monedas en el pipe
+        fd = open(myfifo, O_WRONLY | O_NONBLOCK);
+        write(fd, &monedas_producidas, sizeof(unsigned int));
+        kill(consumidor, SIGUSR1);
+        close(fd);
+        //notifico al consumidor que hay monedas disponibles para leer
+        printf("[Productor]: Consumidor notificado. Esperando señal para continuar...\n");
+        pause();
+    }
+    //cerrar pipe
+    close(fd);
+}
